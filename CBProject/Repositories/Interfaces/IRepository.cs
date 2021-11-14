44c9296a0ce44d88e3bd99905617f669dfc6167d
Gaster89 <jim_gasteratos@hotmail.com>
2021-11-15 01:05:15 +0200

    Create CategoryRepo

--- CBProject/Repositories/Interfaces/IRepository.cs
+++ CBProject/Repositories/Interfaces/IRepository.cs
@@ -5,16 +5,13 @@ namespace CBProject.Repositories.Interfaces
 {
     public interface IRepository<T> // TODO: Implement this interface to every repository
     {
-        void Add(T OBJ);
-        Task<int> AddAsync(T obj);
+        void Add(T obj);
         void Update(T obj);
-        Task<int> UpdateAsync(T obj);
-        void Delete(int id);
-        Task<int> DeleteAsync(int id);
-        T Get(int id);
-        Task<T> GetAsync(int id);
-        T GetEmpty(int id);
-        Task<T> GetEmptyAsync(int id);
+        void Delete(int? id);
+        T Get(int? id);
+        Task<T> GetAsync(int? id);
+        T GetEmpty(int? id);
+        Task<T> GetEmptyAsync(int? id);
         ICollection<T> GetAll();
         Task<ICollection<T>> GetAllAsync();
         ICollection<T> GetAllEmpty();
