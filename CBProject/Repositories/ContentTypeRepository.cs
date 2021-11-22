c65585a20789ae18703daf9fb18d9d7821cb9acb
Gaster89 <jim_gasteratos@hotmail.com>
2021-11-22 21:38:45 +0200

    Add ContentTypeRepository

---
+++ CBProject/Repositories/ContentTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CBProject.Repositories
+{
+    public class ContentTypeRepository
+    {
+    }
+}
