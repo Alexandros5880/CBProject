c1dc08f8c1a1ba39fb8c66f5a9732b21472061d3
Alexandros5880 <Alexpl_15@gmail.com>
2021-12-01 13:47:50 +0200

    Alexandros pull from main

--- CBProject/Migrations/202112011142183_Start.Designer.cs
+++ CBProject/Migrations/202112011142183_Start.Designer.cs
@@ -13,7 +13,7 @@ namespace CBProject.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "202111261703396_Start"; }
+            get { return "202112011142183_Start"; }
         }
         
         string IMigrationMetadata.Source
