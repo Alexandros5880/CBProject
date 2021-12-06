1a4bc7b23d67249c291c553b6e589e937cf9b5fc
Alexandros5880 <Alexpl_15@gmail.com>
2021-12-06 18:59:33 +0200

    Test Commit

--- CBProject/assets/js/app.js
+++ CBProject/assets/js/app.js
@@ -15,40 +15,40 @@ File: Main Js File
     var language = localStorage.getItem('language');
     // Default Language
     var default_lang = 'en';
+    
+    //function setLanguage(lang) {
+    //    if (document.getElementById("header-lang-img")) {
+    //        if (lang == 'en') {
+    //            document.getElementById("header-lang-img").src = "assets/images/flags/us.jpg";
+    //        } else if (lang == 'sp') {
+    //            document.getElementById("header-lang-img").src = "assets/images/flags/spain.jpg";
+    //        }
+    //        else if (lang == 'gr') {
+    //            document.getElementById("header-lang-img").src = "assets/images/flags/germany.jpg";
+    //        }
+    //        else if (lang == 'it') {
+    //            document.getElementById("header-lang-img").src = "assets/images/flags/italy.jpg";
+    //        }
+    //        else if (lang == 'ru') {
+    //            document.getElementById("header-lang-img").src = "assets/images/flags/russia.jpg";
+    //        }
+    //        localStorage.setItem('language', lang);
+    //        language = localStorage.getItem('language');
+    //        getLanguage();
+    //    }
+    //}
 
-    function setLanguage(lang) {
-        if (document.getElementById("header-lang-img")) {
-            if (lang == 'en') {
-                document.getElementById("header-lang-img").src = "assets/images/flags/us.jpg";
-            } else if (lang == 'sp') {
-                document.getElementById("header-lang-img").src = "assets/images/flags/spain.jpg";
-            }
-            else if (lang == 'gr') {
-                document.getElementById("header-lang-img").src = "assets/images/flags/germany.jpg";
-            }
-            else if (lang == 'it') {
-                document.getElementById("header-lang-img").src = "assets/images/flags/italy.jpg";
-            }
-            else if (lang == 'ru') {
-                document.getElementById("header-lang-img").src = "assets/images/flags/russia.jpg";
-            }
-            localStorage.setItem('language', lang);
-            language = localStorage.getItem('language');
-            getLanguage();
-        }
-    }
-
-    // Multi language setting
-    function getLanguage() {
-        (language == null) ? setLanguage(default_lang) : false;
-        $.getJSON('assets/lang/' + language + '.json', function (lang) {
-            $('html').attr('lang', language);
-            $.each(lang, function (index, val) {
-                (index === 'head') ? $(document).attr("title", val['title']) : false;
-                $("[key='" + index + "']").text(val);
-            });
-        });
-    }
+    //// Multi language setting
+    //function getLanguage() {
+    //    (language == null) ? setLanguage(default_lang) : false;
+    //    $.getJSON('assets/lang/' + language + '.json', function (lang) {
+    //        $('html').attr('lang', language);
+    //        $.each(lang, function (index, val) {
+    //            (index === 'head') ? $(document).attr("title", val['title']) : false;
+    //            $("[key='" + index + "']").text(val);
+    //        });
+    //    });
+    //}
 
     function initMetisMenu() {
         //metis menu
