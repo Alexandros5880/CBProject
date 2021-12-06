9a54bff392eaf13f70569858c0cbde547f63051b
theodoragal <theodora_gal@hotmail.com>
2021-12-06 15:03:52 +0200

    Add bootbox - delete modal

--- CBProject/App_Start/BundleConfig.cs
+++ CBProject/App_Start/BundleConfig.cs
@@ -18,12 +18,13 @@ namespace CBProject
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap")
                 .Include(
-                      "~/Scripts/bootstrap.js"
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js"
                       ));
 
             bundles.Add(new StyleBundle("~/Content/css")
--- CBProject/Migrations/202112060841255_Initial.Designer.cs
+++ CBProject/Migrations/202112060841255_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace CBProject.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class InitialMigration : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialMigration));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "202112031456289_InitialMigration"; }
+            get { return "202112060841255_Initial"; }
         }
         
         string IMigrationMetadata.Source
