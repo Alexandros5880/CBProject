169518820a4dcc21f9a08ef5219009b004cfe546
Alexandros5880 <Alexpl_15@gmail.com>
2021-12-06 16:34:26 +0200

    Dashboard tebplate ready!

--- CBProject/DashboardFiles/assets/js/pages/crypto-kyc-app.init.js
+++
@@ -1,5 +0,0 @@
-try {
-    $(document).ready(function () { $("#kyc-verify-wizard").steps({ headerTag: "h3", bodyTag: "section", transitionEffect: "slide" }) });
-} catch (error) {
-    console.log(`Error: ${error}`);
-}
