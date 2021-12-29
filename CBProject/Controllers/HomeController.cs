0c0d9d1253eb707ee617be7728ee36266008053f
Alexandros5880 <Alexpl_15@gmail.com>
2021-12-29 14:57:39 +0200

    Fill Requirements To HomeViewModel.

--- CBProject/Controllers/HomeController.cs
+++ CBProject/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
+using AutoMapper;
 using CBProject.HelperClasses.Interfaces;
+using CBProject.Models;
+using CBProject.Models.EntityModels;
 using CBProject.Models.ViewModels;
 using CBProject.Repositories;
 using CBProject.Repositories.IdentityRepos;
 using CBProject.Repositories.IdentityRepos.Interfaces;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -14,18 +18,30 @@ namespace CBProject.Controllers
         private UsersRepo _usersRepo;
         private CategoriesRepository _categoriesRepo;
         private VideosRepository _videosRepository;
+        private readonly RequirementsRepository _requirementsRepository;
         public HomeController(IUnitOfWork unitOfWork, IUsersRepo usersRepo)
         {
             this._ebooksRepository = unitOfWork.Ebooks;
             this._categoriesRepo = unitOfWork.Categories;
             this._videosRepository = unitOfWork.Videos;
             this._usersRepo = (UsersRepo)usersRepo;
+            this._requirementsRepository = unitOfWork.Requirements;
         }
         public async Task<ActionResult> Index()
         {
+            var ebooks = await this._ebooksRepository.GetAllAsync();
+            var videos = await this._videosRepository.GetAllAsync();
             var viewModel = new HomeViewModel();
-            viewModel.Ebooks = await this._ebooksRepository.GetAllAsync();
-           
+            viewModel.Ebooks = new List<EbookViewModel>();
+            viewModel.Videos = new List<VideoViewModel>();
+            foreach (var ebook in ebooks)
+            {
+                viewModel.Ebooks.Add(Mapper.Map<Ebook, EbookViewModel>(ebook));
+            }
+            foreach(var video in videos)
+            {
+                viewModel.Videos.Add(Mapper.Map<Video, VideoViewModel>(video));
+            }
             return View(viewModel);
         }
         public async Task<ActionResult> About()
