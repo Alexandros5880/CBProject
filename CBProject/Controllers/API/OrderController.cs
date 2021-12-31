a68a617a751f6623964ddf418639007375f42aae
Alexandros5880 <Alexpl_15@gmail.com>
2021-12-31 17:09:34 +0200

    Add API Controllers (Users, Orders).

---
+++ CBProject/Controllers/API/OrderController.cs
@@ -0,0 +1,76 @@
+﻿using CBProject.HelperClasses.Interfaces;
+using CBProject.Models.EntityModels;
+using CBProject.Repositories;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CBProject.Controllers.API
+{
+    public class OrderController : ApiController, IDisposable
+    {
+        private readonly OrdersRepository _ordersRepository;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            this._ordersRepository = unitOfWork.Orders;
+        }
+
+        // GET api/Order
+        public async Task<IHttpActionResult> Get()
+        {
+            var orders = await this._ordersRepository.GetAllAsync();
+            return Ok(orders);
+        }
+
+        // GET api/Order/5
+        public async Task<IHttpActionResult> Get(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var order = await this._ordersRepository.GetAsync(id);
+            if (order == null)
+                return NotFound();
+            return Ok(order);
+        }
+
+        // POST api/Order
+        public async Task<IHttpActionResult> Post([FromBody] Order order)
+        {
+            if (order == null)
+                return NotFound();
+            this._ordersRepository.Add(order);
+            await this._ordersRepository.SaveAsync();
+            return Ok(order);
+        }
+
+        // PUT api/Order/5
+        public async Task<IHttpActionResult> Put([FromBody] Order order)
+        {
+            if (order == null)
+                return NotFound();
+            this._ordersRepository.Update(order);
+            await this._ordersRepository.SaveAsync();
+            return Ok(order);
+        }
+
+        // DELETE api/Order/5
+        public async Task<IHttpActionResult> Delete(int id)
+        {
+            if (id == null)
+                return NotFound();
+            await this._ordersRepository.DeleteAsync(id);
+            await this._ordersRepository.SaveAsync();
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this._ordersRepository.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CBProject/Controllers/API/UserController.cs
@@ -0,0 +1,84 @@
+﻿using CBProject.Models;
+using CBProject.Repositories.IdentityRepos;
+using CBProject.Repositories.IdentityRepos.Interfaces;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CBProject.Controllers.API
+{
+    public class UserController : ApiController, IDisposable
+    {
+        private readonly UsersRepo _usersRepo;
+
+        public UserController(IUsersRepo usersRepo)
+        {
+            this._usersRepo = (UsersRepo)usersRepo;
+        }
+
+        // GET api/User
+        public async Task<IHttpActionResult> Get()
+        {
+            var users = await this._usersRepo.GetAllAsync();
+            return Ok(users);
+        }
+
+        // GET api/User/5
+        public async Task<IHttpActionResult> Get(string id)
+        {
+            if (id == null)
+                return NotFound();
+            var user = await this._usersRepo.GetAsync(id);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+
+        // POST api/User
+        public async Task<IHttpActionResult> Post([FromBody] ApplicationUser user)
+        {
+            if (user == null)
+                return NotFound();
+            await this._usersRepo.AddAsync(user);
+            return Ok(user);
+        }
+
+        // PUT api/User/5
+        public async Task<IHttpActionResult> Put([FromBody] ApplicationUser user)
+        {
+            if (user == null)
+                return NotFound();
+            await this._usersRepo.UpdateAsync(user);
+            return Ok(user);
+        }
+
+        // DELETE api/User/5
+        public async Task<IHttpActionResult> Delete(string id)
+        {
+            if (id == null)
+                return NotFound();
+            await this._usersRepo.DeleteAsync(id);
+            return Ok();
+        }
+
+        // DELETE api/User/real/5
+        [HttpDelete]
+        [Route("api/User/real/{id}")]
+        public async Task<IHttpActionResult> DeleteReal(string id)
+        {
+            if (id == null)
+                return NotFound();
+            await this._usersRepo.DeleteRealAsync(id);
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this._usersRepo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
