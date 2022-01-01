1e8f30248cf536ce3391317bf0c8d0ee10270773
Alexandros5880 <Alexpl_15@gmail.com>
2022-01-01 03:43:16 +0200

    Add ReviewsToEbooksRepository && ReviewsToVideosRepository.

--- CBProject/Repositories/ReviewsToEbooksRepository.cs
+++ CBProject/Repositories/ReviewsToEbooksRepository.cs
@@ -4,6 +4,7 @@ using CBProject.Models.EntityModels;
 using CBProject.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,119 +13,123 @@ namespace CBProject.Repositories
     public class ReviewsToEbooksRepository : IRepository<ReviewToEbook>, IDisposable
     {
         private bool disposedValue;
-
         private readonly ApplicationDbContext _context;
-
         public ReviewsToEbooksRepository(IUnitOfWork unitOfWork)
         {
             this._context = unitOfWork.Context;
         }
-
         public void Add(ReviewToEbook obj)
         {
             if (obj == null)
                 throw new ArgumentNullException(nameof(obj));
-
-            throw new NotImplementedException();
+            this._context.ReviewsToEbooks.Add(obj);
         }
-
         public void Delete(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = this._context.ReviewsToEbooks
+                                .FirstOrDefault(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            this._context.ReviewsToEbooks.Remove(obj);
         }
-
         public async Task DeleteAsync(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = await this._context.ReviewsToEbooks
+                                            .FirstOrDefaultAsync(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            this._context.ReviewsToEbooks.Remove(obj);
         }
-
         public ReviewToEbook Get(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = this._context.ReviewsToEbooks
+                                    .Include(r => r.Ebook)
+                                    .Include(r => r.Review)
+                                    .FirstOrDefault(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public ICollection<ReviewToEbook> GetAll()
         {
-            throw new NotImplementedException();
+            return this._context.ReviewsToEbooks
+                                .Include(r => r.Ebook)
+                                .Include(r => r.Review)
+                                .ToList();
         }
-
         public async Task<ICollection<ReviewToEbook>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await this._context.ReviewsToEbooks
+                                .Include(r => r.Ebook)
+                                .Include(r => r.Review)
+                                .ToListAsync();
         }
-
         public ICollection<ReviewToEbook> GetAllEmpty()
         {
-            throw new NotImplementedException();
+            return this._context.ReviewsToEbooks
+                                        .ToList();
         }
-
         public async Task<ICollection<ReviewToEbook>> GetAllEmptyAsync()
         {
-            throw new NotImplementedException();
+            return await this._context.ReviewsToEbooks
+                                        .ToListAsync();
         }
-
         public IQueryable<ReviewToEbook> GetAllQueryable()
         {
-            throw new NotImplementedException();
+            return this._context.ReviewsToEbooks;
         }
-
         public async Task<ReviewToEbook> GetAsync(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = this._context.ReviewsToEbooks
+                                    .Include(r => r.Ebook)
+                                    .Include(r => r.Review)
+                                    .FirstOrDefault(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public ReviewToEbook GetEmpty(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = this._context.ReviewsToEbooks
+                                    .FirstOrDefault(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public async Task<ReviewToEbook> GetEmptyAsync(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = await this._context.ReviewsToEbooks
+                                            .FirstOrDefaultAsync(R => R.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public void Save()
         {
-            throw new NotImplementedException();
+            this._context.SaveChanges();
         }
-
         public async Task<int> SaveAsync()
         {
-            throw new NotImplementedException();
+            return await this._context.SaveChangesAsync();
         }
-
         public void Update(ReviewToEbook obj)
         {
             if (obj == null)
                 throw new ArgumentNullException(nameof(obj));
-
-            throw new NotImplementedException();
+            this._context.Entry(obj).State = EntityState.Modified;
         }
-
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
--- CBProject/Repositories/ReviewsToVideosRepository.cs
+++ CBProject/Repositories/ReviewsToVideosRepository.cs
@@ -4,6 +4,7 @@ using CBProject.Models.EntityModels;
 using CBProject.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,119 +13,121 @@ namespace CBProject.Repositories
     public class ReviewsToVideosRepository : IRepository<ReviewToVideo>, IDisposable
     {
         private bool disposedValue;
-
         private readonly ApplicationDbContext _context;
-
         public ReviewsToVideosRepository(IUnitOfWork unitOfWork)
         {
             this._context = unitOfWork.Context;
         }
-
         public void Add(ReviewToVideo obj)
         {
             if (obj == null)
                 throw new ArgumentNullException(nameof(obj));
-
-            throw new NotImplementedException();
+            this._context.ReviewsToVideos.Add(obj);
         }
-
         public void Delete(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = this._context.ReviewsToVideos
+                                    .FirstOrDefault(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            this._context.ReviewsToVideos.Remove(obj);
         }
-
         public async Task DeleteAsync(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = await this._context.ReviewsToVideos
+                                        .FirstOrDefaultAsync(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            this._context.ReviewsToVideos.Remove(obj);
         }
-
         public ReviewToVideo Get(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = this._context.ReviewsToVideos
+                                    .FirstOrDefault(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public ICollection<ReviewToVideo> GetAll()
         {
-            throw new NotImplementedException();
+            return this._context.ReviewsToVideos
+                                .Include(r => r.Video)
+                                .Include(r => r.Review)
+                                .ToList();
         }
-
         public async Task<ICollection<ReviewToVideo>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await this._context.ReviewsToVideos
+                                        .Include(r => r.Video)
+                                        .Include(r => r.Review)
+                                        .ToListAsync();
         }
-
         public ICollection<ReviewToVideo> GetAllEmpty()
         {
-            throw new NotImplementedException();
+            return this._context.ReviewsToVideos
+                                .ToList();
         }
-
         public async Task<ICollection<ReviewToVideo>> GetAllEmptyAsync()
         {
-            throw new NotImplementedException();
+            return await this._context.ReviewsToVideos
+                                        .ToListAsync();
         }
-
         public IQueryable<ReviewToVideo> GetAllQueryable()
         {
-            throw new NotImplementedException();
+            return this._context.ReviewsToVideos;
         }
-
         public async Task<ReviewToVideo> GetAsync(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = await this._context.ReviewsToVideos
+                                            .Include(r => r.Video)
+                                            .Include(r => r.Review)
+                                            .FirstOrDefaultAsync(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public ReviewToVideo GetEmpty(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = this._context.ReviewsToEbooks
+                            .FirstOrDefault(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public async Task<ReviewToVideo> GetEmptyAsync(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-
-
-            throw new NotImplementedException();
+            var obj = await this._context.ReviewsToVideos
+                                        .FirstOrDefaultAsync(r => r.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public void Save()
         {
-            throw new NotImplementedException();
+            this._context.SaveChanges();
         }
-
         public async Task<int> SaveAsync()
         {
-            throw new NotImplementedException();
+            return await this._context.SaveChangesAsync();
         }
-
         public void Update(ReviewToVideo obj)
         {
             if (obj == null)
                 throw new ArgumentNullException(nameof(obj));
-
-            throw new NotImplementedException();
+            this._context.Entry(obj).State = EntityState.Modified;
         }
-
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
