b7eb2e88e589f34b6aebb76bcae1e9ef0f8f2b9d
Alexandros5880 <Alexpl_15@gmail.com>
2022-01-16 16:50:26 +0200

    MessMessageRepository OK.

--- CBProject/Areas/Messenger/Repositories/MesMessagesRepository.cs
+++ CBProject/Areas/Messenger/Repositories/MesMessagesRepository.cs
@@ -4,6 +4,7 @@ using CBProject.HelperClasses.Interfaces;
 using CBProject.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,103 +14,126 @@ namespace CBProject.Areas.Messenger.Repositories
     {
         private bool disposedValue;
         private readonly ApplicationDbContext _context;
-
         public MesMessagesRepository(IUnitOfWork unitOfWork)
         {
             this._context = unitOfWork.Context;
         }
-
         public void Add(MessengerMessage obj)
         {
             if (obj == null)
                 throw new ArgumentNullException(nameof(obj));
-            throw new System.NotImplementedException();
+            this._context.MessengerMessages.Add(obj);
         }
-
         public void Delete(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-            throw new System.NotImplementedException();
+            var obj = this._context.MessengerMessages
+                        .Include(m => m.Group)
+                        .Include(m => m.User)
+                        .FirstOrDefault(g => g.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            this._context.MessengerMessages.Remove(obj);
         }
-
-        public Task DeleteAsync(int? id)
+        public async Task DeleteAsync(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-            throw new System.NotImplementedException();
+            var obj = await this._context.MessengerMessages
+                        .Include(m => m.Group)
+                        .Include(m => m.User)
+                        .FirstOrDefaultAsync(g => g.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            this._context.MessengerMessages.Remove(obj);
         }
-
         public MessengerMessage Get(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-            throw new System.NotImplementedException();
+            var obj = this._context.MessengerMessages
+                        .Include(m => m.Group)
+                        .Include(m => m.User)
+                        .FirstOrDefault(g => g.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public ICollection<MessengerMessage> GetAll()
         {
-            throw new System.NotImplementedException();
+            return this._context.MessengerMessages
+                        .Include(m => m.Group)
+                        .Include(m => m.User)
+                        .ToList();
         }
-
-        public Task<ICollection<MessengerMessage>> GetAllAsync()
+        public async Task<ICollection<MessengerMessage>> GetAllAsync()
         {
-            throw new System.NotImplementedException();
+            return await this._context.MessengerMessages
+                        .Include(m => m.Group)
+                        .Include(m => m.User)
+                        .ToListAsync();
         }
-
         public ICollection<MessengerMessage> GetAllEmpty()
         {
-            throw new System.NotImplementedException();
+            return this._context.MessengerMessages
+                                    .ToList();
         }
-
-        public Task<ICollection<MessengerMessage>> GetAllEmptyAsync()
+        public async Task<ICollection<MessengerMessage>> GetAllEmptyAsync()
         {
-            throw new System.NotImplementedException();
+            return await this._context.MessengerMessages
+                                        .ToListAsync();
         }
-
         public IQueryable<MessengerMessage> GetAllQueryable()
         {
-            throw new System.NotImplementedException();
+            return this._context.MessengerMessages;
         }
-
-        public Task<MessengerMessage> GetAsync(int? id)
+        public async Task<MessengerMessage> GetAsync(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-            throw new System.NotImplementedException();
+            var obj = await this._context.MessengerMessages
+                        .Include(m => m.Group)
+                        .Include(m => m.User)
+                        .FirstOrDefaultAsync(g => g.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public MessengerMessage GetEmpty(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-            throw new System.NotImplementedException();
+            var obj = this._context.MessengerMessages
+                        .FirstOrDefault(g => g.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
-        public Task<MessengerMessage> GetEmptyAsync(int? id)
+        public async Task<MessengerMessage> GetEmptyAsync(int? id)
         {
             if (id == null)
                 throw new ArgumentNullException(nameof(id));
-            throw new System.NotImplementedException();
+            var obj = await this._context.MessengerMessages
+                        .FirstOrDefaultAsync(g => g.ID == id);
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return obj;
         }
-
         public void Save()
         {
-            throw new System.NotImplementedException();
+            this._context.SaveChanges();
         }
-
-        public Task<int> SaveAsync()
+        public async Task<int> SaveAsync()
         {
-            throw new System.NotImplementedException();
+            return await this._context.SaveChangesAsync();
         }
-
         public void Update(MessengerMessage obj)
         {
             if (obj == null)
                 throw new ArgumentNullException(nameof(obj));
-            throw new System.NotImplementedException();
+            this._context.Entry(obj).State = EntityState.Modified;
         }
-
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
