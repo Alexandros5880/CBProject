3de5e9d063f9bbd7aa075f770c653406bb6e5e09
Alexandros5880 <Alexpl_15@gmail.com>
2022-01-17 13:42:56 +0200

    Messenger GroupController (API) OK.

--- CBProject/Areas/Messenger/Controllers/API/GroupController.cs
+++ CBProject/Areas/Messenger/Controllers/API/GroupController.cs
@@ -1,39 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using CBProject.Areas.Forum.Models.EntityModels;
+using CBProject.Areas.Messenger.Repositories;
+using CBProject.HelperClasses.Interfaces;
+using System;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace CBProject.Areas.Messenger.Controllers.API
 {
-    public class GroupController : ApiController
+    public class GroupController : ApiController, IDisposable
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        private readonly MesGroupsRepository _mesGroupsRepository;
+
+        public GroupController(IUnitOfWork unitoOfWork)
         {
-            return new string[] { "value1", "value2" };
+            this._mesGroupsRepository = unitoOfWork.MessengerGroups;
         }
 
-        // GET api/<controller>/5
-        public string Get(int id)
+        // GET api/Group
+        public async Task<IHttpActionResult> Get()
         {
-            return "value";
+            var obj = await this._mesGroupsRepository.GetAllEmptyAsync();
+            return Ok(obj);
         }
 
-        // POST api/<controller>
-        public void Post([FromBody] string value)
+        // GET api/Group/5
+        public async Task<IHttpActionResult> Get(int? id)
         {
+            if (id == null)
+                return NotFound();
+            var obj = await this._mesGroupsRepository.GetEmptyAsync(id);
+            if (obj == null)
+                return NotFound();
+            return Ok(obj);
         }
 
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody] string value)
+        // POST api/Group
+        public async Task<IHttpActionResult> Post([FromBody] MessengerGroup obj)
         {
+            if (obj == null)
+                return NotFound();
+            this._mesGroupsRepository.Add(obj);
+            await this._mesGroupsRepository.SaveAsync();
+            return Ok(obj);
         }
 
-        // DELETE api/<controller>/5
-        public void Delete(int id)
+        // PUT api/Group/5
+        public async Task<IHttpActionResult> Put([FromBody] MessengerGroup obj)
         {
+            if (obj == null)
+                return NotFound();
+            this._mesGroupsRepository.Update(obj);
+            await this._mesGroupsRepository.SaveAsync();
+            return Ok(obj);
+        }
+
+        // DELETE api/Group/5
+        public async Task<IHttpActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            await this._mesGroupsRepository.DeleteAsync(id);
+            await this._mesGroupsRepository.SaveAsync();
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this._mesGroupsRepository.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
